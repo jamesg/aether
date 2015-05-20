520d0f0bf7e9010ebd87183632840c40f97e22d9
James Goode <james@uwcs.co.uk>
2015-05-20 16:47:45 +0100

    Arduino sketch transmits moisture level and temperature.

--- sensor/sketch.cpp
+++ sensor/sketch.cpp
@@ -1,79 +1,85 @@
 #include "Arduino.h"
 
+#include "LiquidCrystal.h"
 #include "OneWire.h"
 
-OneWire ds(8);
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+OneWire ds(10);
 
-void setup(void) {
-  Serial.begin(9600);
+void setup(void)
+{
+    lcd.begin(16,2);
+    lcd.print("Plant Monitor");
+    pinMode(8, OUTPUT);
+    digitalWrite(8, HIGH);
+    Serial.begin(115200);
 }
 
-void loop(void) {
-  // One-wire read
-  byte i;
-  byte present = 0;
-  byte data[12];
-  byte addr[8];
-
-  ds.reset_search();
-  if ( !ds.search(addr)) {
-      Serial.print("No more addresses.\n");
-      ds.reset_search();
-      return;
-  }
-
-  Serial.print("R=");
-  for( i = 0; i < 8; i++) {
-    Serial.print(addr[i], HEX);
-    Serial.print(" ");
-  }
-
-  if ( OneWire::crc8( addr, 7) != addr[7]) {
-      Serial.print("CRC is not valid!\n");
-      return;
-  }
-
-  if ( addr[0] == 0x10) {
-      Serial.print("Device is a DS18S20 family device.\n");
-  }
-  else if ( addr[0] == 0x28) {
-      Serial.print("Device is a DS18B20 family device.\n");
-  }
-  else {
-      Serial.print("Device family is not recognized: 0x");
-      Serial.println(addr[0],HEX);
-      return;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44,1);         // start conversion, with parasite power on at the end
-
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
-  Serial.print("P=");
-  Serial.print(present,HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print( OneWire::crc8( data, 8), HEX);
-  Serial.println();
-  
-  // Moisture read.
-  delay(100);
-  int moisture_val = analogRead(0);
-  Serial.print("moisture value = ");
-  Serial.print(moisture_val, DEC);
-  Serial.println();
+int decode_moisture()
+{
+    int moisture_val = analogRead(0);
+    return moisture_val;
 }
 
+void decode_temperature(int *Whole, int *Fract)
+{
+    byte i;
+    byte present = 0;
+    byte data[12];
+    byte addr[8];
+
+    ds.reset_search();
+    if(!ds.search(addr)) {
+        ds.reset_search();
+        return;
+    }
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);         // start conversion, parasite power
+
+    delay(1000);     // maybe 750ms is enough, maybe not
+
+    present = ds.reset();
+    ds.select(addr);
+    ds.write(0xBE);         // Read Scratchpad
+
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+        data[i] = ds.read();
+    }
+    int HighByte, LowByte, TReading, SignBit, Tc_100;
+    LowByte = data[0];
+    HighByte = data[1];
+    TReading = (HighByte << 8) + LowByte;
+    SignBit = TReading & 0x8000;  // test most sig bit
+    if(SignBit)
+        TReading = (TReading ^ 0xffff) + 1;
+    Tc_100 = (6 * TReading) + TReading / 4;
+    *Whole = Tc_100 / 100;
+    *Fract = Tc_100 % 100;
+}
+
+void log_string(const char *str)
+{
+    Serial.print(str);
+    lcd.clear();
+    lcd.print(str);
+}
+
+void loop()
+{
+    char buf[16];
+
+    int moisture_val = decode_moisture();
+    snprintf(buf, sizeof(buf), "moisture = %d\n", moisture_val);
+    log_string(buf);
+
+    int Whole, Fract;
+    decode_temperature(&Whole, &Fract);
+
+    snprintf(buf, sizeof(buf), "temp = %d.%d\n", Whole, Fract);
+    log_string(buf);
+
+    delay(1000);
+}
 
