da21a11006b630177420a0042ab00f97c146f1a8
James Goode <james@uwcs.co.uk>
2015-05-26 10:28:36 +0100

    Add phases to web interface.

--- web/models.js
+++ web/models.js
@@ -17,16 +17,38 @@ var Phase = RestModel.extend(
         }
         );
 var PhaseCollection = RestCollection.extend(
-        {
-            model: Phase,
-            url: restUri('phase'),
-            setOrder: function() {
-                this.each(
-                    function(model, i) {
-                        model.set('phase_order', i);
-                    }
-                    );
-            }
+    {
+        model: Phase,
+        url: restUri('phase'),
+        save: function(options) {
+            this.setOrder();
+            if(_.isUndefined(options))
+                options = {};
+            return this.sync('update', this, options);
+        },
+        setOrder: function() {
+            this.each(
+                function(model, i) {
+                    model.set('phase_order', i);
+                }
+                );
+        },
+        // Swap elements at index i and index i + 1.
+        swap: function(i) {
+            var model = this.at(i);
+            this.remove(model);
+            this.add(model, { at: i + 1 });
+        },
+        moveUp: function(model) {
+            var i = this.indexOf(model);
+            if(i >= 1 && i < this.length)
+                this.swap(i - 1);
+        },
+        moveDown: function(model) {
+            var i = this.indexOf(model);
+            if(i >= 0 && i < this.length - 1)
+                this.swap(i);
         }
-        );
+    }
+    );
 
--- web/settings.js
+++ web/settings.js
@@ -3,6 +3,20 @@ var SettingsPage = PageView.extend(
         pageTitle: 'Settings',
         initialize: function() {
             PageView.prototype.initialize.apply(this, arguments);
+            PageView.prototype.render.apply(this);
+            var phases = new PhaseCollection;
+            phases.fetch();
+            (new CollectionView({
+                model: phases,
+                el: this.$('p[name=current-phases]'),
+                view: StaticView.extend({
+                    tagName: 'span',
+                    className: 'tag',
+                    template: '<%-phase_desc%>'
+                })
+            })).render();
+        },
+        render: function() {
         },
         template: $('#settingspage-template').html(),
         events: {
@@ -10,6 +24,29 @@ var SettingsPage = PageView.extend(
         },
         showPhases: function() {
             gApplication.pushPage(PhasesPage);
+        }
+    }
+    );
+
+var PhaseForm = StaticView.extend(
+    {
+        template: $('#phaseform-template').html(),
+        save: function() {
+            this.model.set({
+                phase_desc: this.$('input[name=phase_desc]').val()
+            });
+            this.model.save(
+                {},
+                {
+                    success: (function() {
+                        this.trigger('finished');
+                    }).bind(this)
+                }
+                )
+        },
+        initialize: function() {
+            StaticView.prototype.initialize.apply(this, arguments);
+            this.on('save', this.save.bind(this));
         }
     }
     );
@@ -23,8 +60,9 @@ var PhasesPage = PageView.extend(
 
             this._phases = new PhaseCollection;
             this._phases.fetch();
+            var phases = this._phases;
 
-            (new CollectionView({
+            this._phasesView = new CollectionView({
                 el: this.$('ul[name=phases]'),
                 view: StaticView.extend({
                     tagName: 'li',
@@ -33,30 +71,70 @@ var PhasesPage = PageView.extend(
                     <span class="visible-on-hover">\
                     <button type="button" name="up">Move up</button>\
                     <button type="button" name="down">Move down</button>\
-                    <button type="button" name="hide">Delete</button>\
+                    <button type="button" name="destroy">Delete</button>\
                     <button type="button" name="edit">Edit</button>\
                     </span>\
                     ',
-                events: {
-                    'click button[name=up]': 'moveUp',
-                    'click button[name=down]': 'moveDown',
-                    'click button[name=hide]': 'hide',
-                    'click button[name=edit]': 'edit',
-                    'click span[name=phase-desc]': 'edit'
-                },
-                edit: function() {
-                    var m = new Modal({
-                        modal: this.model,
-                        view: StaticView.extend({
-                            template: $('#phaseform-template').html()
-                        }),
-                        buttons: [ StandardButton.cancel(), StandardButton.save(function() { console.log('todo save phase'); }) ]
-                    });
-                    gApplication.modal(m);
-                }
+                    events: {
+                        'click button[name=up]': 'moveUp',
+                        'click button[name=down]': 'moveDown',
+                        'click button[name=hide]': 'hide',
+                        'click button[name=edit]': 'edit',
+                        'click button[name=destroy]': 'destroy',
+                        'click span[name=phase-desc]': 'edit'
+                    },
+                    moveUp: function() {
+                        phases.moveUp(this.model);
+                        phases.save();
+                    },
+                    moveDown: function() {
+                        phases.moveDown(this.model);
+                        phases.save();
+                    },
+                    edit: function() {
+                        var m = new Modal({
+                            model: this.model,
+                            view: PhaseForm,
+                            buttons: [
+                                StandardButton.cancel(),
+                                StandardButton.save()
+                            ]
+                        });
+                        gApplication.modal(m);
+                    },
+                    destroy: function() {
+                        var m = new ConfirmModal({
+                            callback: (function() {
+                                this.model.destroy();
+                            }).bind(this),
+                            message: 'Are you sure you want to delete ' +
+                                this.model.get('phase_desc') + '?'
+                        });
+                        gApplication.modal(m);
+                    }
                 }),
                 model: this._phases
-            })).render();
+            });
+            this._phasesView.render();
+        },
+        events: {
+            'click button[name=new-phase]': 'create'
+        },
+        create: function() {
+            var m = new Modal({
+                model: new Phase,
+                view: PhaseForm,
+                buttons: [ StandardButton.cancel(), StandardButton.create() ]
+            });
+            gApplication.modal(m);
+            this.listenTo(
+                    m,
+                    'finished',
+                    function() {
+                        this._phases.fetch(),
+                        this._phases.save()
+                    }
+                    );
         },
         render: function() {},
         template: $('#phasespage-template').html(),
