2e7d91b3f7036a87337760ee7c7c2e01a83987db
James Goode <james@uwcs.co.uk>
2015-06-29 14:33:57 +0100

    Weather forecast for the next three days.

--- src/weather_router.cpp
+++ src/weather_router.cpp
@@ -7,6 +7,44 @@
 #include "hades/custom_select_one.hpp"
 #include "hades/join.hpp"
 
+namespace
+{
+    int to_unix_time(boost::posix_time::ptime t)
+    {
+        return (
+            t -
+            boost::posix_time::ptime(boost::gregorian::date(1970, 1, 1))
+        ).total_seconds();
+    }
+    styx::list forecast_range(
+        hades::connection& conn,
+        boost::posix_time::ptime from,
+        boost::posix_time::ptime to
+    )
+    {
+        return hades::equi_outer_join<
+            aether::forecast,
+            aether::forecast_main,
+            aether::forecast_clouds,
+            aether::forecast_rain,
+            aether::forecast_weather,
+            aether::forecast_wind>(
+            conn,
+            hades::filter(
+                hades::where(
+                    "aether_forecast.forecast_dt >= ? AND "
+                    "aether_forecast.forecast_dt < ? ",
+                    hades::row<int, int>(
+                        to_unix_time(from),
+                        to_unix_time(to)
+                    )
+                ),
+                hades::order_by("aether_forecast.forecast_dt ASC")
+                )
+        );
+    }
+}
+
 aether::weather_router::weather_router(hades::connection& conn) {
     install<int>(
         atlas::http::matcher("/([0-9]+)", "GET"),
@@ -74,35 +112,38 @@ aether::weather_router::weather_router(hades::connection& conn) {
                 timezone_offset_s =
                     boost::lexical_cast<int>(params.find("timezone")->second) * 60;
             return atlas::http::json_response(
-                hades::equi_outer_join<
-                    forecast,
-                    forecast_main,
-                    forecast_clouds,
-                    forecast_rain,
-                    forecast_weather,
-                    forecast_wind>(
-                        conn,
-                        hades::filter(
-                            hades::where(
-                                "aether_forecast.forecast_dt >= ? AND "
-                                "aether_forecast.forecast_dt < ? ",
-                                hades::row<int, int>(
-                                (
-                                    boost::posix_time::ptime(boost::gregorian::day_clock::universal_day()) -
-                                    boost::posix_time::ptime(boost::gregorian::date(1970, 1, 1))
-                                    ).total_seconds() + timezone_offset_s,
-                                (
-                                    boost::posix_time::ptime(boost::gregorian::day_clock::universal_day()) +
-                                    boost::gregorian::days(1) -
-                                    boost::posix_time::ptime(boost::gregorian::date(1970, 1, 1))
-                                    ).total_seconds() + timezone_offset_s
-                                    )
-                            ),
-                            hades::order_by("aether_forecast.forecast_dt ASC")
-                            )
-                        )
-                );
+                forecast_range(
+                    conn,
+                    boost::posix_time::ptime(boost::gregorian::day_clock::universal_day()) +
+                        boost::posix_time::seconds(timezone_offset_s),
+                    boost::posix_time::ptime(boost::gregorian::day_clock::universal_day()) +
+                        boost::gregorian::days(1) +
+                        boost::posix_time::seconds(timezone_offset_s)
+                )
+            );
+        }
+        );
+    install_get<int>(
+        atlas::http::matcher("/day/([0-9]+)", "GET"),
+        [&conn](std::map<std::string, std::string> params, int day) {
+            int timezone_offset_s = 0;
+            if(params.find("timezone") == params.end())
+                atlas::log::warning("aether::weather_router") <<
+                    "no timezone specified; using 0 (UTC)";
+            else
+                timezone_offset_s =
+                    boost::lexical_cast<int>(params.find("timezone")->second) * 60;
+            return atlas::http::json_response(
+                forecast_range(
+                    conn,
+                    boost::posix_time::ptime(boost::gregorian::day_clock::universal_day()) +
+                        boost::gregorian::days(day) +
+                        boost::posix_time::seconds(timezone_offset_s),
+                    boost::posix_time::ptime(boost::gregorian::day_clock::universal_day()) +
+                        boost::gregorian::days(day + 1) +
+                        boost::posix_time::seconds(timezone_offset_s)
+                )
+            );
         }
         );
 }
-
--- web/weather.js
+++ web/weather.js
@@ -3,106 +3,145 @@ var kelvinToCelsius = function(degK) {
 };
 
 var WeatherPage = PageView.extend(
-        {
-            pageTitle: 'Weather',
-            template: $('#weatherpage-template').html(),
-            initialize: function() {
-                PageView.prototype.initialize.apply(this, arguments);
-                PageView.prototype.render.apply(this);
+    {
+        pageTitle: 'Weather',
+        template: $('#weatherpage-template').html(),
+        gotoDay: function(day) {
+            this.$('h1[name=weather-title]').html(
+                'Weather for ' +
+                moment().add(day, 'days').format('dddd Do MMMM YYYY')
+                );
+            this._forecast.fetch({
+                // Provide the timezone offset in minutes so that the
+                // server will only provide weather for one day at the
+                // local timezone.
+                url: restUri(
+                    'weather/day/' + day + '?timezone=' +
+                    (new Date).getTimezoneOffset()
+                    ),
+                success: this._initializeCharts.bind(this)
+            });
+        },
+        initialize: function() {
+            PageView.prototype.initialize.apply(this, arguments);
+            PageView.prototype.render.apply(this);
 
-                this._forecast = new ForecastCollection;
-                this._forecast.fetch({
-                    // Provide the timezone offset in minutes so that the
-                    // server will only provide weather for one day at the
-                    // local timezone.
-                    url: restUri(
-                             'weather/today?timezone=' +
-                             (new Date).getTimezoneOffset()
-                             ),
-                    success: this._initializeCharts.bind(this)
-                });
+            var location = new Location;
+            location.fetch();
+            (new StaticView({
+                el: this.$('div[name=location]'),
+                template: 'Today\'s weather forecast for <%-location_city%> (<%-location_lat%>&deg; N, <%-location_lon%>&deg; E).',
+                model: location
+            })).render();
 
-                var location = new Location;
-                location.fetch();
+            this._forecast = new ForecastCollection;
+            this.gotoDay(0);
 
-                (new StaticView({
-                    el: this.$('div[name=location]'),
-                    template: 'Today\'s weather forecast for <%-location_city%> (<%-location_lat%>&deg; N, <%-location_lon%>&deg; E).',
-                    model: location
-                })).render();
-            },
-            _initializeCharts: function() {
-                (new StaticView({
-                    el: this.$('div[name=summary]'),
-                    templateParams: function() {
-                        return {
-                            maxTemp: kelvinToCelsius(
-                                    _.max(this.model.pluck('forecast_main_temp'))
-                                    ).toFixed(1),
-                            minTemp: kelvinToCelsius(
-                                    _.min(this.model.pluck('forecast_main_temp'))
-                                    ).toFixed(1)
-                        };
+            var page = this;
+            (new CollectionView({
+                el: this.$('ul[name=days]'),
+                view: StaticView.extend({
+                    tagName: 'li',
+                    template: '<%-date%>',
+                    events: {
+                        click: 'selectDay'
                     },
-                    template: '\
-                    <h2>Summary</h2>\
-                    Min: <%-minTemp%> &deg;C, max: <%-maxTemp%> &deg;C\
-                    ',
-                    model: this._forecast
-                })).render();
-                var temperatureSeries = this._forecast.map(
-                    function(point) {
-                        return {
-                            x: moment(point.get('forecast_dt'), 'X').format('HH:mm'),
-                            y: Number(kelvinToCelsius(point.get('forecast_main_temp')).toFixed(1))
-                        };
+                    selectDay: function() {
+                        console.log('select day', this.model.get('day'));
+                        page.gotoDay(this.model.get('day'));
                     }
-                    );
-                var rainSeries = this._forecast.map(
-                    function(point) {
-                        return {
-                            x: moment(point.get('forecast_dt'), 'X').format('HH:mm'),
-                            y: Number(point.get('forecast_rain'))
-                        };
+                }),
+                model: new (Backbone.Collection.extend({
+                    model: Backbone.Model.extend({
+                        defaults: {
+                            day: 0,
+                            date: 'invalid'
+                        }
+                    })
+                }))(
+                    _.map(
+                        _.range(0, 3),
+                        function(day) {
+                            return {
+                                day: day,
+                                date: moment().add(day, 'days').format('dddd Do')
+                            };
+                        }
+                    )
+                )
+            }));
+        },
+        _initializeCharts: function() {
+            (new StaticView({
+                el: this.$('div[name=summary]'),
+                templateParams: function() {
+                    return {
+                        maxTemp: kelvinToCelsius(
+                                _.max(this.model.pluck('forecast_main_temp'))
+                                ).toFixed(1),
+                        minTemp: kelvinToCelsius(
+                                _.min(this.model.pluck('forecast_main_temp'))
+                                ).toFixed(1)
+                    };
+                },
+                template: '\
+                <h2>Summary</h2>\
+                Min: <%-minTemp%> &deg;C, max: <%-maxTemp%> &deg;C\
+                ',
+                model: this._forecast
+            })).render();
+            var temperatureSeries = this._forecast.map(
+                function(point) {
+                    return {
+                        x: moment(point.get('forecast_dt'), 'X').format('HH:mm'),
+                        y: Number(kelvinToCelsius(point.get('forecast_main_temp')).toFixed(1))
+                    };
+                }
+                );
+            var rainSeries = this._forecast.map(
+                function(point) {
+                    return {
+                        x: moment(point.get('forecast_dt'), 'X').format('HH:mm'),
+                        y: Number(point.get('forecast_rain'))
+                    };
+                }
+                );
+            var temperatureChartData = {
+                xScale: 'ordinal',
+                yScale: 'linear',
+                type: 'line',
+                main: [
+                    {
+                        className: '.temperaturechartdata',
+                        interpolation: 'linear',
+                        data: temperatureSeries
                     }
-                    );
-                var temperatureChartData = {
-                    xScale: 'ordinal',
-                    yScale: 'linear',
-                    type: 'line',
-                    main: [
-                        {
-                            className: '.temperaturechartdata',
-                            interpolation: 'linear',
-                            data: temperatureSeries
-                        }
-                    ]
-                };
-                var rainChartData = {
-                    xScale: 'ordinal',
-                    yScale: 'linear',
-                    type: 'bar',
-                    main: [
-                        {
-                            className: '.temperaturechartdata',
-                            interpolation: 'linear',
-                            data: rainSeries
-                        }
-                    ]
-                };
-                if(_.has(this, '_temperatureChart'))
-                    this._temperatureChart.setData(temperatureChartData);
-                else
-                    this._temperatureChart =
-                        new xChart('line', temperatureChartData, '#temperaturechart');
-                if(_.has(this, '_rainChart'))
-                    this._rainChart.setData(rainChartData);
-                else
-                    this._rainChart =
-                        new xChart('bar', rainChartData, '#rainchart');
-            },
-            render: function() {
-            }
+                ]
+            };
+            var rainChartData = {
+                xScale: 'ordinal',
+                yScale: 'linear',
+                type: 'bar',
+                main: [
+                    {
+                        className: '.temperaturechartdata',
+                        interpolation: 'linear',
+                        data: rainSeries
+                    }
+                ]
+            };
+            if(_.has(this, '_temperatureChart'))
+                this._temperatureChart.setData(temperatureChartData);
+            else
+                this._temperatureChart =
+                    new xChart('line', temperatureChartData, '#temperaturechart');
+            if(_.has(this, '_rainChart'))
+                this._rainChart.setData(rainChartData);
+            else
+                this._rainChart =
+                    new xChart('bar', rainChartData, '#rainchart');
+        },
+        render: function() {
         }
-        );
-
+    }
+    );
