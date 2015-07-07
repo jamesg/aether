b8fd116f57f49e97c1e68e5bb0704c3595e59aba
James Goode <james@uwcs.co.uk>
2015-07-07 08:33:28 +0100

    Add more temperature model tests.

--- src/test/temperature_model.cpp
+++ src/test/temperature_model.cpp
@@ -5,99 +5,231 @@
 #include "hades/join.hpp"
 #include "temperature_model.hpp"
 
-SCENARIO("aether::temperature_model") {
+namespace
+{
+    class database_fixture
+    {
+    public:
+        database_fixture() :
+            m_connection(hades::connection::in_memory_database())
+        {
+            aether::db::create(conn());
+
+            // Create a batch.
+            m_batch.save(conn());
+
+            // Move the default sensor to the batch.
+            aether::sensor default_sensor(hades::get_one<aether::sensor>(conn()));
+            aether::sensor_at_batch sab;
+            sab.set_id(m_batch.id());
+            sab.get_int<aether::attr::sensor_id>() =
+                default_sensor.get_int<aether::attr::sensor_id>();
+            sab.save(conn());
+        }
+        hades::connection& conn()
+        {
+            return m_connection;
+        }
+        aether::batch batch()
+        {
+            return m_batch;
+        }
+    private:
+        hades::connection m_connection;
+        aether::batch m_batch;
+    };
+
     const int base_time = 1436086800;
-    GIVEN("a database") {
-        hades::connection conn(hades::connection::in_memory_database());
+}
 
-        aether::db::create(conn);
+SCENARIO("aether::temperature_model") {
+    // A forecast in which only the temperature changes.
+    GIVEN("a contrived forecast and log") {
+        database_fixture fixture;
+        hades::connection& conn = fixture.conn();
+        {
+            aether::forecast f;
+            aether::forecast_main fm;
+            aether::forecast_clouds fc;
+            aether::temperature_log tl;
+            f.get_int<aether::attr::forecast_dt>() =
+                fm.get_int<aether::attr::forecast_dt>() =
+                fc.get_int<aether::attr::forecast_dt>() =
+                tl.get_double<aether::attr::log_time>() =
+                base_time;
+            fm.get_double<aether::attr::forecast_main_temp>() = 20.0;
+            fc.get_int<aether::attr::forecast_clouds_all>() = 50;
+            tl.get_int<aether::attr::batch_id>() =
+                fixture.batch().get_int<aether::attr::batch_id>();
+            tl.get_double<aether::attr::temperature>() = 21.0;
+            f.save(conn);
+            fm.save(conn);
+            fc.save(conn);
+            tl.save(conn);
+        }
 
-        // Create a batch.
-        aether::batch batch;
-        batch.save(conn);
+        {
+            aether::forecast f;
+            aether::forecast_main fm;
+            aether::forecast_clouds fc;
+            aether::temperature_log tl;
+            f.get_int<aether::attr::forecast_dt>() =
+                fm.get_int<aether::attr::forecast_dt>() =
+                fc.get_int<aether::attr::forecast_dt>() =
+                tl.get_double<aether::attr::log_time>() =
+                base_time + 10800;
+            fm.get_double<aether::attr::forecast_main_temp>() = 22.0;
+            fc.get_int<aether::attr::forecast_clouds_all>() = 50;
+            tl.get_int<aether::attr::batch_id>() =
+                fixture.batch().get_int<aether::attr::batch_id>();
+            tl.get_double<aether::attr::temperature>() = 23.0;
+            f.save(conn);
+            fm.save(conn);
+            fc.save(conn);
+            tl.save(conn);
+        }
 
-        // Move the default sensor to the batch.
-        aether::sensor default_sensor(hades::get_one<aether::sensor>(conn));
-        aether::sensor_at_batch sab;
-        sab.set_id(batch.id());
-        sab.get_int<aether::attr::sensor_id>() =
-            default_sensor.get_int<aether::attr::sensor_id>();
-        sab.save(conn);
+        aether::temperature_model model(conn);
 
-        GIVEN("a contrived forecast and log") {
-            {
-                aether::forecast f;
-                aether::forecast_main fm;
-                aether::forecast_clouds fc;
-                aether::temperature_log tl;
-                f.get_int<aether::attr::forecast_dt>() =
-                    fm.get_int<aether::attr::forecast_dt>() =
-                    fc.get_int<aether::attr::forecast_dt>() =
-                    tl.get_double<aether::attr::log_time>() =
-                    base_time;
-                fm.get_double<aether::attr::forecast_main_temp>() = 20.0;
-                fc.get_int<aether::attr::forecast_clouds_all>() = 50;
-                tl.get_int<aether::attr::batch_id>() =
-                    batch.get_int<aether::attr::batch_id>();
-                tl.get_double<aether::attr::temperature>() = 21.0;
-                f.save(conn);
-                fm.save(conn);
-                fc.save(conn);
-                tl.save(conn);
-            }
+        // Test on seen data.
+        // There are only two points, so the model should fit perfectly.
+        styx::list seen(
+            hades::equi_outer_join<
+                aether::forecast,
+                aether::forecast_main,
+                aether::forecast_clouds>(
+                conn,
+                hades::order_by("aether_forecast.forecast_dt ASC")
+            )
+        );
+        REQUIRE(seen.size() == 2);
+        float estimate_cmp;
+        estimate_cmp = model.estimate(styx::object(seen.at(0))) / 21;
+        REQUIRE(estimate_cmp > 0.99);
+        REQUIRE(estimate_cmp < 1.01);
+        estimate_cmp = model.estimate(styx::object(seen.at(1))) / 23;
+        REQUIRE(estimate_cmp > 0.99);
+        REQUIRE(estimate_cmp < 1.01);
 
-            {
-                aether::forecast f;
-                aether::forecast_main fm;
-                aether::forecast_clouds fc;
-                aether::temperature_log tl;
-                f.get_int<aether::attr::forecast_dt>() =
-                    fm.get_int<aether::attr::forecast_dt>() =
-                    fc.get_int<aether::attr::forecast_dt>() =
-                    tl.get_double<aether::attr::log_time>() =
-                    base_time + 10800;
-                fm.get_double<aether::attr::forecast_main_temp>() = 22.0;
-                fc.get_int<aether::attr::forecast_clouds_all>() = 50;
-                tl.get_int<aether::attr::batch_id>() =
-                    batch.get_int<aether::attr::batch_id>();
-                tl.get_double<aether::attr::temperature>() = 23.0;
-                f.save(conn);
-                fm.save(conn);
-                fc.save(conn);
-                tl.save(conn);
-            }
+        // Test on unseen data.
+        {
+            aether::forecast_main fm;
+            fm.get_double<aether::attr::forecast_main_temp>() = 21.0;
+            fm.get_int<aether::attr::forecast_clouds_all>() = 50;
+            // A temperature between the two in the training set should
+            // result in an estimate between the two in the temperature log.
+            REQUIRE(model.estimate(fm) > 21);
+            REQUIRE(model.estimate(fm) < 23);
+        }
+    }
 
-            aether::temperature_model model(conn);
+    // A forecast in which both the temperature an cloud cover change.  The
+    // measured temperature will be greater when there is less cloud cover
+    // (inspired by a sensor placed in a greenhouse).
+    GIVEN("a more complex forecast") {
+        database_fixture fixture;
+        hades::connection& conn = fixture.conn();
+        {
+            aether::forecast f;
+            aether::forecast_main fm;
+            aether::forecast_clouds fc;
+            aether::temperature_log tl;
+            f.get_int<aether::attr::forecast_dt>() =
+                fm.get_int<aether::attr::forecast_dt>() =
+                fc.get_int<aether::attr::forecast_dt>() =
+                tl.get_double<aether::attr::log_time>() =
+                base_time;
+            fm.get_double<aether::attr::forecast_main_temp>() = 20.0;
+            fc.get_int<aether::attr::forecast_clouds_all>() = 100;
+            tl.get_int<aether::attr::batch_id>() =
+                fixture.batch().get_int<aether::attr::batch_id>();
+            tl.get_double<aether::attr::temperature>() = 20.0;
+            f.save(conn);
+            fm.save(conn);
+            fc.save(conn);
+            tl.save(conn);
+        }
 
-            // Test on seen data.
-            styx::list seen(
-                hades::equi_outer_join<
-                    aether::forecast,
-                    aether::forecast_main,
-                    aether::forecast_clouds>(
-                    conn,
-                    hades::order_by("aether_forecast.forecast_dt ASC")
-                )
-            );
-            REQUIRE(seen.size() == 2);
-            float estimate_cmp;
-            estimate_cmp = model.estimate(styx::object(seen.at(0))) / 21;
-            REQUIRE(estimate_cmp > 0.99);
-            REQUIRE(estimate_cmp < 1.01);
-            estimate_cmp = model.estimate(styx::object(seen.at(1))) / 23;
-            REQUIRE(estimate_cmp > 0.99);
-            REQUIRE(estimate_cmp < 1.01);
+        {
+            aether::forecast f;
+            aether::forecast_main fm;
+            aether::forecast_clouds fc;
+            aether::temperature_log tl;
+            f.get_int<aether::attr::forecast_dt>() =
+                fm.get_int<aether::attr::forecast_dt>() =
+                fc.get_int<aether::attr::forecast_dt>() =
+                tl.get_double<aether::attr::log_time>() =
+                base_time + 10800;
+            fm.get_double<aether::attr::forecast_main_temp>() = 21.0;
+            fc.get_int<aether::attr::forecast_clouds_all>() = 0;
+            tl.get_int<aether::attr::batch_id>() =
+                fixture.batch().get_int<aether::attr::batch_id>();
+            tl.get_double<aether::attr::temperature>() = 27.0;
+            f.save(conn);
+            fm.save(conn);
+            fc.save(conn);
+            tl.save(conn);
+        }
 
-            // Test on unseen data.
-            {
-                aether::forecast_main fm;
-                fm.get_double<aether::attr::forecast_main_temp>() = 21.0;
-                fm.get_int<aether::attr::forecast_clouds_all>() = 50;
-                // A temperature between the two in the training set should
-                // result in an estimate between the two in the temperature log.
-                REQUIRE(model.estimate(fm) > 21);
-                REQUIRE(model.estimate(fm) < 23);
-            }
+        {
+            aether::forecast f;
+            aether::forecast_main fm;
+            aether::forecast_clouds fc;
+            aether::temperature_log tl;
+            f.get_int<aether::attr::forecast_dt>() =
+                fm.get_int<aether::attr::forecast_dt>() =
+                fc.get_int<aether::attr::forecast_dt>() =
+                tl.get_double<aether::attr::log_time>() =
+                base_time + 21600;
+            fm.get_double<aether::attr::forecast_main_temp>() = 22.0;
+            fc.get_int<aether::attr::forecast_clouds_all>() = 50;
+            tl.get_int<aether::attr::batch_id>() =
+                fixture.batch().get_int<aether::attr::batch_id>();
+            tl.get_double<aether::attr::temperature>() = 25.0;
+            f.save(conn);
+            fm.save(conn);
+            fc.save(conn);
+            tl.save(conn);
+        }
+
+        aether::temperature_model model(conn);
+
+        // Test on seen data.
+        styx::list seen(
+            hades::equi_outer_join<
+                aether::forecast,
+                aether::forecast_main,
+                aether::forecast_clouds>(
+                conn,
+                hades::order_by("aether_forecast.forecast_dt ASC")
+            )
+        );
+        REQUIRE(seen.size() == 3);
+        // 0: 20, 1: 27, 2: 25
+        REQUIRE(model.estimate(styx::object(seen.at(0))) > 19);
+        REQUIRE(model.estimate(styx::object(seen.at(0))) < 21);
+        REQUIRE(model.estimate(styx::object(seen.at(1))) > 26);
+        REQUIRE(model.estimate(styx::object(seen.at(1))) < 28);
+        REQUIRE(model.estimate(styx::object(seen.at(2))) > 24);
+        REQUIRE(model.estimate(styx::object(seen.at(2))) < 26);
+
+        // Test on unseen data.
+        {
+            aether::forecast_main fm;
+            fm.get_double<aether::attr::forecast_main_temp>() = 20.0;
+            fm.get_int<aether::attr::forecast_clouds_all>() = 75;
+            // The measured temperature should be greater with less cloud cover.
+            REQUIRE(model.estimate(fm) > 20);
+            REQUIRE(model.estimate(fm) < 23);
+        }
+        {
+            aether::forecast_main fm;
+            fm.get_double<aether::attr::forecast_main_temp>() = 21.0;
+            fm.get_int<aether::attr::forecast_clouds_all>() = 25;
+            // The measured temperature should be cooler than 27 because one
+            // training sample had forecast=21, measured=27 with no cloud cover.
+            REQUIRE(model.estimate(fm) > 21);
+            REQUIRE(model.estimate(fm) < 27);
         }
     }
 }
